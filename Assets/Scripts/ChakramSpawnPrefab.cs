e79efec47768141c60d0a21e7247ff8fe3d9caa9
sauravpunalekar <sauravpunalekar@gmail.com>
2017-06-23 17:15:49 +0530

    Added force to x axis

--- Assets/Scripts/ChakramSpawnPrefab.cs
+++ Assets/Scripts/ChakramSpawnPrefab.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.UI;
 
 public class ChakramSpawnPrefab : MonoBehaviour {
 	Object g;
 	GameObject g1;
+	public Slider s;
 	void Start () {
 		g = AssetDatabase.LoadAssetAtPath ("Assets/Cartoon_Weapon_Pack/Prefab/Shield_03.prefab", typeof(GameObject));
 			g1 = Instantiate (g, new Vector3 (250, 5, 5),Quaternion.Euler(180,0,0))as GameObject;
@@ -15,7 +17,9 @@ public class ChakramSpawnPrefab : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp ("space")) {
-			g1.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, 0, 8000));
+			float a = s.value;
+
+			g1.GetComponent<Rigidbody> ().AddForce (new Vector3 (a*250, 0, 8000));
 
 		}
 		if (g1.transform.position.z > 15.0f) {
---
+++ Assets/Scripts/SliderMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderMove : MonoBehaviour {
+	public Slider s;
+	int count = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count % 2 == 0) {
+			if (s.value < s.maxValue) {
+				moveup ();
+			} else {
+				count++;
+			}
+		}
+		if (count % 2 == 1) {
+			if (s.value > s.minValue) {
+				movedown ();
+			} else {
+				count++;
+			}
+		}
+	}
+	void moveup()
+	{
+		s.value += 1;
+	}
+	void movedown()
+	{
+		s.value -= 1;
+	}
+}
