ad92743d7715dced7012333fef102f56b61ff423
Akshay Shah <akshah1997@gmail.com>
2017-06-26 17:34:23 +0530

    Level Up and You Win
    Added the code to show level up on 1st and 2nd levels and show You Win on completion of last level

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ScoreDisplay : MonoBehaviour {
 	public static Text myScore;
 	public static bool isCollision = false;
+	int endScore;
 	// Use this for initialization
 	void Start () {
 		myScore = GameObject.Find ("Canvas/ScoreDisplay").GetComponent<Text> ();
@@ -16,7 +17,17 @@ public class ScoreDisplay : MonoBehaviour {
 		if (!isCollision) {
 		} else {
 			if (myScore.fontSize < 300) {
-				myScore.text = ScoreCount.score.ToString ();
+				if (ScoreCount.score == 0 || ScoreCount.score == 9) {
+					endScore = ScoreCount.reqScore - 2;
+					if (ScoreCount.score == 0) {
+						myScore.text = "Level Up!";
+					} else {
+						myScore.text = "You Win";
+					}
+				} else {
+					myScore.text = ScoreCount.score.ToString ();
+				}
+
 				myScore.fontSize += 11;
 			} else {
 				isCollision = false;
