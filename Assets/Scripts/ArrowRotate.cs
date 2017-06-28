fdf088ce055719c3910e81f3a0bed57b9f74032a
sauravpunalekar <sauravpunalekar@gmail.com>
2017-06-28 14:54:29 +0530

    Beta Build xD

---
+++ Assets/Scripts/ArrowRotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotate : MonoBehaviour {
+	int flag =0;
+	public GameObject g;
+	// Use this for initialization
+	void Start () {
+		//g = GameObject.Find ("Arrow");
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (flag==0) {
+			MoveRight ();
+		
+			if (g.transform.rotation.eulerAngles.y >= 32 && g.transform.rotation.eulerAngles.y < 34 ) {			
+				flag = 1;
+			}
+		}
+		if (flag == 1) {
+			MoveLeft ();
+			if (g.transform.rotation.eulerAngles.y <= 328 && g.transform.rotation.eulerAngles.y > 326) {			
+				flag = 0;
+			}
+		}
+		/*Debug.Log (g.transform.rotation.eulerAngles.y);
+		if (g.transform.rotation.eulerAngles.y > 32 || g.transform.rotation.eulerAngles.y < 328) {
+			Debug.Log ("change flag");
+			flag = -flag;
+		}
+		Debug.Log (flag);
+		MoveRight ();*/
+	}
+	void MoveRight (){
+		g.transform.Rotate(0,g.transform.rotation.y+1,0);
+		//g.transform.Rotate(0,g.transform.rotation.y+flag,0);
+	}
+	void MoveLeft(){
+		g.transform.Rotate(0,g.transform.rotation.y-1,0);
+		return;	
+	}
+}
--- Assets/Scripts/ChakramSpawnPrefab.cs
+++ Assets/Scripts/ChakramSpawnPrefab.cs
@@ -8,6 +8,7 @@ public class ChakramSpawnPrefab : MonoBehaviour {
 	Object g;
 	GameObject g1;
 	public Slider s;
+	public GameObject g2;
 	void Start () {
 		g = AssetDatabase.LoadAssetAtPath ("Assets/Cartoon_Weapon_Pack/Prefab/Shield_03.prefab", typeof(GameObject));
 			g1 = Instantiate (g, new Vector3 (250, 5, 5),Quaternion.Euler(180,0,0))as GameObject;
@@ -15,7 +16,7 @@ public class ChakramSpawnPrefab : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	/*void Update () {
 		if (Input.GetKeyUp ("space")) {
 			float a = s.value;
 
@@ -25,6 +26,20 @@ public class ChakramSpawnPrefab : MonoBehaviour {
 		if (g1.transform.position.z > 15.0f) {
 			g1 = Instantiate (g, new Vector3 (250, 5, 5),Quaternion.Euler(180,0,0))as GameObject;
 		}
+	}*/
+	void Update(){
+		if (Input.GetKeyUp ("space")) {
+			float y = g2.transform.rotation.eulerAngles.y;
+			if (y > 300) {
+			
+				y = y-360;
+			}
+			g1.GetComponent<Rigidbody> ().AddForce (new Vector3 (y*250, 0, 8000));
+		}
+		if (g1.transform.position.z > 15.0f) {
+			g1 = Instantiate (g, new Vector3 (250, 5, 5),Quaternion.Euler(180,0,0))as GameObject;
+		}
+
 	}
 
 
