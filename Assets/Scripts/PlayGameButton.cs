94c963ffb71b07a28df4da65fbec1c1e00121eb2
Akshay Shah <akshah1997@gmail.com>
2018-06-09 21:20:32 +0530

    Added Initial game scene that contains basic information about Chakram

---
+++ Assets/Scripts/PlayGameButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayGameButton : MonoBehaviour
+{
+    public Button playGame;
+
+    // Use this for initialization
+    private void Start()
+    {
+        playGame.onClick.AddListener(delegate { LoadGameScene(); });
+    }
+
+    private void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
